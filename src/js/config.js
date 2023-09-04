a6cff8c10ec502c167d05d3e766bd22dfbdd9663
sidarth-23 <sidarth157@gmail.com>
2023-09-04 21:44:44 +0530

    Added API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'fb4ad3bf-96a2-4cdc-ae51-7cd50a57f43d';
 export const MODAL_CLOSE_SEC = 2.5;
